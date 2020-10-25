6cbe58a6fb564dd9513988f030d1ad8ee8a7a71b
Yuliya Reutovich <yrevtovich@mail.ru>
2020-10-25 22:30:28 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = [];
+  const closeBrackets = [];
+  const result= [];
+  const arr = str.split('');
+
+  bracketsConfig.map((item) => {
+    openBrackets.push(item[0]);
+    closeBrackets.push(item[1]);
+  });  
+
+  for (let i = 0; i < arr.length; i++) {
+    const openBraket = openBrackets[openBrackets.indexOf(arr[i])];
+    const closeBraket = closeBrackets[closeBrackets.indexOf(arr[i])];
+    const pairBraket = openBrackets[closeBrackets.indexOf(arr[i])];
+
+    if ((openBraket === closeBraket && result[result.length-1] === openBraket) 
+    || closeBraket && result[result.length-1] === pairBraket) {
+      result.pop();
+      continue;
+    }
+
+    result.push(arr[i]);
+  }
+
+  return result.length !== 0 ? false : true;
 }
